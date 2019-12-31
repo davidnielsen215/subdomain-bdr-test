f8c5bf6be2a0f2fbd93177ab1fe7648969a96680
David Nielsen <davidnielsen215@gmail.com>
2019-12-31 11:57:38 -0700

    posting test subdomain to github

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './BDR_LOGO.png'
 import './App.css';
+
+
 
 function App() {
   return (
@@ -8,15 +10,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Subdomain Best Deal Retailer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="https://seetheit.com/"
+          target="blank"
         >
-          Learn React
+          Back to Home Page
         </a>
       </header>
     </div>
